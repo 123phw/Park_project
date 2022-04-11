557b0b012230f1e01c4c8c1ce0ceb990a0ecac96
jaehii <withfavor98@naver.com>
2022-04-12 03:04:07 +0900

    build: config구현

---
+++ src/main/java/com/example/parkprjct/security/TestSecurityConfig.java
@@ -0,0 +1,20 @@
+package com.example.parkprjct.security;
+
+import com.project.parkproject.domain.user.UserService;
+import com.project.parkproject.security.filter.TestTokenFilter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Profile("local")
+@Configuration
+@EnableWebSecurity
+@Slf4j
+public class TestSecurityConfig extends SecurityConfigBase {
+    public TestSecurityConfig(UserService userService) {
+        super(new TestTokenFilter(userService));
+    }
+}
