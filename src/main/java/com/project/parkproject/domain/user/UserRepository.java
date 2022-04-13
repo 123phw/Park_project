d2367c865d1e6d4060df988bd2c54b3b739b42ef
고동휘 <asd236678@gmail.com>
2022-04-13 20:28:22 +0900

    User 삭제 Users로 변경

--- src/main/java/com/project/parkproject/domain/user/UserRepository.java
+++ src/main/java/com/project/parkproject/domain/user/UserRepository.java
@@ -1,7 +1,11 @@
 package com.project.parkproject.domain.user;
 
+import java.util.Optional;
+import com.project.parkproject.entity.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
-public interface UserRepository extends JpaRepository<User, String> {
-
+@Repository
+public interface UserRepository extends JpaRepository<Users, Long> {
+    Optional<Users> findBygId(String gId);
 }
