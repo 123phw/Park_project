dfd4c19d70304a72c6847a35cc448a10f5e1bc32
hyewonPark <123pho@naver.com>
2022-04-23 16:02:43 +0900

    feat:리뷰작성api

---
+++ src/main/java/com/example/parkprjct/dto/ReviewSaveResponseDto.java
@@ -0,0 +1,23 @@
+package com.example.parkprjct.dto;
+
+import com.example.parkprjct.entity.Review;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+public class ReviewSaveResponseDto {
+
+    private int rRate;
+    private String rDesc;
+    Long pIdx;
+    Long uIdx;
+
+    public ReviewSaveResponseDto(Review review){
+        this.rDesc = review.getRDesc();
+        this.rRate = review.getRRate();
+        this.uIdx = review.getUIdx().getUIdx();
+        this.pIdx = review.getPIdx().getPIdx();
+
+    }
+}
