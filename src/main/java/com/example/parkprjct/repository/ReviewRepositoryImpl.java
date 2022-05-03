55995fd4a6d1407be83922bc9f991596ea459731
hyewonPark <123pho@naver.com>
2022-05-03 15:32:41 +0900

    feat:리뷰 최신순 정렬

--- src/main/java/com/example/parkprjct/repository/ReviewRepositoryImpl.java
+++ src/main/java/com/example/parkprjct/repository/ReviewRepositoryImpl.java
@@ -25,19 +25,11 @@ public class ReviewRepositoryImpl extends QuerydslRepositorySupport implements R
     @Override
     public Page<Review> reviewList(Long pIdx, Pageable pageable){
 
-//        List<Review> query = queryFactory.selectFrom(review)
-//                .where(review.pIdx.pIdx.eq(pIdx)).fetch();
-
         JPQLQuery<Review> query = queryFactory.selectFrom(review)
                 .where(review.pIdx.pIdx.eq(pIdx))
                 .orderBy(review.rDate.desc());
 
         List<Review> reviews = this.getQuerydsl().applyPagination(pageable, query).fetch();
-//        Long total = queryFactory
-//                .select(review.count())
-//                .from(review)
-//                .where(review.pIdx.pIdx.eq(pIdx))
-//                .fetchOne();
 
         return new PageImpl<>(reviews, pageable, query.fetchCount());
 
